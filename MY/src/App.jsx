46a4d3c73dc206eccf412da5838124a5a435e486
mugaggadotcom <marynabakka94@gmail.com>
2026-06-30 08:21:56 +0300

    added my react assignment

---
+++ MY/src/App.jsx
@@ -0,0 +1,14 @@
+function App ()
+  {
+    return(
+      <div>
+      <h1> MY FAVORITE THINGS.</h1>
+      <h2> NABAKKA MARY </h2>
+       <p> My fovorite food: chips, fried eggs and liver.</p>
+      <p> My hobby: listening to music.</p>
+      <p>My favorite movie: Baby's Day Out.</p>
+      <p> My dream career: Web development and designing.</p>
+      </div>
+    );
+  }
+export default App;
